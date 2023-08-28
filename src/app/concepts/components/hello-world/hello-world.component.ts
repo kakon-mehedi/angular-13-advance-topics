5bf2229b25757bce62af21ea87ed1a40829a01c3
kakon-selise <kakon.mehedi@selise.ch>
2023-08-28 10:12:35 +0600

    Type and Interface implements by class

--- src/app/concepts/components/hello-world/hello-world.component.ts
+++ src/app/concepts/components/hello-world/hello-world.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-hello-world',
-  templateUrl: './hello-world.component.html',
-  styleUrls: ['./hello-world.component.scss']
+	selector: 'app-hello-world',
+	templateUrl: './hello-world.component.html',
+	styleUrls: ['./hello-world.component.scss'],
 })
-export class HelloWorldComponent implements OnInit {
+export class HelloWorldComponent implements OnInit, Iuser, Student {
+	constructor() {}
+	ngOnInit(): void {}
 
-  constructor() { }
+	/* Implementing Student Type */
 
-  ngOnInit(): void {
-  }
+	school: string = 'Ekhlas';
+	roll: number = 7;
+	resultSum = (num1: number, num2: number) => {
+		return num1 + num2;
+	};
 
+	/* Implementing Iuser Interface */
+
+	myname = 'Kakon';
+	age: number = 26;
+
+	arithmatic = function (x: number, y: number) {
+		return x + y;
+	};
+	stringFn = (str: string) => str.toUpperCase();
+
+	/* Implementing BmiFn Interface */
+
+	BMI: BmiFn = (w: number, h: number) => {
+		return w / h;
+	};
+}
+
+export interface Iuser {
+	myname: string;
+	age: number;
+	arithmatic: (x: number, y: number) => number;
+	stringFn: (str: string) => string;
+}
+
+export type Student = {
+	school: string;
+	roll: number;
+	resultSum: (gpa1: number, gpa2: number) => number;
+};
+
+export interface BmiFn {
+	(wight: number, height: number): number;
 }
