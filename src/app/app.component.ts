12bfc2814da9992e06d4ecd4cb12c53117c7da80
kakon-selise <kakon.mehedi@selise.ch>
2024-08-15 22:45:07 +0600

    feat: Add switchmap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { filter, interval, of, switchMap, take } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'fresh-angular-13';
+export class AppComponent implements OnInit {
+	constructor(private http: HttpClient) {}
+
+	ngOnInit(): void {
+
+		/**
+		 * Creating ids observable by interval creational operator 
+		 */
+		const ids = interval(1).pipe(
+			filter((id) => id > 0),  // Filter value from ids
+			take(5) // only take 5 values from ids
+		);
+
+		ids.pipe(
+			switchMap((id) => {
+				return this.http.get(
+					`https://jsonplaceholder.typicode.com/todos/${id}`
+				);
+			})
+		).subscribe((data) => console.log(data));
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './concepts/components/hello-world/hello-world.component';
 import { MaterialModule } from './shared/modules/material.module';
-import { RxjsOperatorsAtAGlanceComponent } from './concepts/components/rxjs-operators-at-a-glance/rxjs-operators-at-a-glance.component';
+
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HelloWorldComponent,
-		RxjsOperatorsAtAGlanceComponent,
 	],
 	imports: [
 		BrowserModule,
