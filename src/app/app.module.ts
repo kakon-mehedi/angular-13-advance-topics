7af02c28add8f702fc9f39fa93f8b0d690668a79
kakon-selise <kakon.mehedi@selise.ch>
2024-08-19 10:58:56 +0600

    feat: Add basic files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './concepts/components/hello-world/hello-world.component';
-import { MaterialModule } from './shared/modules/material.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
